90f63642fa14e83a3f09ead5b069b79831e396ea
Rasmus <Rasmus@vinperlen.dk>
2021-01-26 11:18:00 +0100

    Add project files.

---
+++ Array og Matematik/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_og_Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opg A");
+            int[] listeB = new int[11];
+            for (int i = 0, even = 0; i < listeB.Length; i++, even += 2)
+            {
+                listeB[i] = even;
+                if (listeB[i] % 3 != 0)
+                {
+                    Console.WriteLine(listeB[i]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Opg B");
+            listeB[2] = 17;
+
+            for (int i = 0, even = 0; i < listeB.Length; i++, even += 2)
+            {
+                listeB[i] = even;
+                if (listeB[i] % 3 == 0)
+                {
+                    Console.WriteLine(listeB[i]);
+                }
+            }
+
+        }
+    }
+}
